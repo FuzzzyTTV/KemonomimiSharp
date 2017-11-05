22360559d362baa8eea52fd24a240a6cf7c24cf0
MrHypercube <xarkingaming@gmail.com>
2017-11-05 15:17:20 -0800

    Finished

---
+++ KemonomimiSharp/Kemonomimi.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace KemonomimiSharp
+{
+    public class Kemonomimi
+    {
+        public String Name;
+        public String Type;
+        public String Gender;
+        public float Weight;
+        public bool LikesYou;
+        public int Patted;
+        public int Points;
+
+        public void LogName()
+        {
+            Console.WriteLine(Name);
+        }
+
+
+        public void LogType()
+        {
+            Console.WriteLine(Type);
+        }
+
+
+        public void LogGender()
+        {
+            Console.WriteLine(Gender);
+        }
+
+
+        public void LogWeight()
+        {
+            Console.WriteLine(Weight);
+        }
+
+
+        public void LogLikes()
+        {
+            Console.WriteLine(LikesYou);
+        }
+
+
+        public void LogTimesPatted()
+        {
+            Console.WriteLine(Patted);
+        }
+
+        public void LogAll()
+        {
+            Console.WriteLine(Name);
+            Console.WriteLine(Type);
+            Console.WriteLine(Gender);
+            Console.WriteLine(Weight + " Pounds");
+        }
+
+        public void Headpat()
+        {
+            Test.Patted = Test.Patted + 1;
+
+            if (Patted <= 2)
+            {
+                Points = Points + 1;
+            }
+
+
+            if (Patted > 2)
+            {
+                Points = Points - 1;
+                Console.WriteLine(Name + " is starting to get kind of annoyed");
+            }
+
+
+            if (Type == "Fox" || Type == "fox" || Type == "Kitsune" || Type == "kitsune" || Type == "Bunny" || Type == "bunny" || Type == "Rabbit" || Type == "rabbit")
+            {
+
+                if (Patted < 2)
+                {
+                    Console.WriteLine(Name+ " Is really loving you right now!");
+                }
+
+            }
+
+
+            if (Type == "Cat" || Type == "cat" || Type == "Neko" || Type == "neko")
+            {
+                Console.WriteLine(Name + " has started purring!");
+            }
+
+
+            if (Type == "Dog" || Type == "dog" || Type == "Wolf" || Type == "wolf" || Type == "Okami" || Type == "okami")
+            {
+                Console.WriteLine(Name + " has started panting in happyness!");
+            }
+
+
+        }
+
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using KemonomimiSharp;
+
+namespace Test
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Kemonomimi Test = new Kemonomimi();
+
+            Test.Name = "Kitsune";
+            Test.Type = "fox";
+            Test.Gender = "Female";
+            Test.Weight = 200.00f;
+
+            Test.LogAll();
+
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Would you like to headpat " + Test.Name + "?");
+
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+
+            Console.WriteLine("A: Yes");
+
+            Console.WriteLine(" ");
+
+            Console.WriteLine("B: Yes");
+
+            Console.WriteLine(" ");
+
+            Console.WriteLine("P: Get the ammount of times patted");
+                    
+            Console.WriteLine(" ");
+
+            start:
+
+            String x = Console.ReadLine();
+
+            if (x == "A" || x == "a")
+            {
+                Test.Headpat();
+                Test.Patted = Test.Patted + 1;
+                goto start;
+
+            }
+
+            if (x == "B" || x == "b")
+            {
+                Console.WriteLine("Ok then I guess.");
+                goto start;
+            }
+
+            if (x == "P" || x == "p")
+            {
+                Console.WriteLine(Test.Patted);
+                goto start;
+            }
+        }
+    }
+}
